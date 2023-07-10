f070e6c46f68e72a22731413278ef1ca7798fc37
Swapnil639 <swapnilpatil631997@gmail.com>
2023-07-10 21:36:49 +0530

    Added :Gambler start with stake of 100 everyday and bet 1 every game

--- src/com/bridgelabz/GamblingGame.java
+++ src/com/bridgelabz/GamblingGame.java
@@ -3,5 +3,11 @@ package com.bridgelabz;
 public class GamblingGame {
     public static void main(String[] args) {
         System.out.println("Welcome to Gambling Simulation Program");
+        double stake = 100.0; // starting stake
+        double bet = 1.0; // bet amount
+        int gamesPlayed = 0; // number of games played
+        int gamesWon = 0; // number of games won
+
+        System.out.println("Stake of everyday:"+stake+" Bet every game:"+bet);
     }
 }
