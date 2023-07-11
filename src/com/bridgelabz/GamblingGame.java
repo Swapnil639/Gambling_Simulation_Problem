dc239c867d42746bb0ec9f525038fedbdff01fda
Swapnil639 <swapnilpatil631997@gmail.com>
2023-07-11 10:28:02 +0530

    Added :as a calculate gamble if won or loss 50% of the stake

--- src/com/bridgelabz/GamblingGame.java
+++ src/com/bridgelabz/GamblingGame.java
@@ -4,7 +4,7 @@ import java.util.Random;
 
 public class GamblingGame {
     public static int stakeOfEveryDay = 100;
-    public static final int betEveryGame = 1;
+    public static final int Bet_Every_Game = 1;
     public static final int win = 1;
 
     public static void main(String[] args) {
@@ -13,15 +13,21 @@ public class GamblingGame {
     }
 
     public static void playGame() {
-        Random random=new Random();
-        int bit=random.nextInt(2);
-        if (bit == win) {
-            System.out.println("You are win");
-            stakeOfEveryDay++;
-        } else {
-            System.out.println("You are Loss");
-            stakeOfEveryDay--;
+        while (stakeOfEveryDay > 50 && stakeOfEveryDay < 150) {
+            int bit = (int) (Math.random() * (2) + 1);
+            if (bit == win) {
+                System.out.println("You are win");
+                stakeOfEveryDay++;
+            } else {
+                System.out.println("You are Loss");
+                stakeOfEveryDay--;
+            }
+            System.out.println("Now your stake is " + stakeOfEveryDay);
         }
-        System.out.println("Now your stake is " + stakeOfEveryDay);
+        if (stakeOfEveryDay == 50){
+            System.out.println("You have loss 50% stake");
+        }else {
+            System.out.println("You Have Win 50% stake");
+        }
     }
 }
