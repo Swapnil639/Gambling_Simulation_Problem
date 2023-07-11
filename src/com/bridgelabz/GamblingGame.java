3615a38c4846b2d77c68853df7bc384bbfa9e0e0
Swapnil639 <swapnilpatil631997@gmail.com>
2023-07-11 11:19:30 +0530

    Added :Each month would like to know the days won and lost

--- src/com/bridgelabz/GamblingGame.java
+++ src/com/bridgelabz/GamblingGame.java
@@ -2,9 +2,10 @@ package com.bridgelabz;
 
 public class GamblingGame {
     public static int stakeOfEveryDay = 100;
-    public static final int Bet_Every_Game = 1;
-    public static final int Days = 20;
-    public static final int win = 1;
+    public static int winDays = 0;
+    public static int lossDays = 0;
+    public static final int DAYS = 20;
+    public static final int WIN = 1;
 
     public static void main(String[] args) {
         System.out.println("Welcome the Gambling Simulation Problem");
@@ -12,23 +13,30 @@ public class GamblingGame {
     }
 
     public static void playGame() {
-        for (int i = 1; i <= Days; i++) {
+        for (int i = 1; i <= DAYS; i++) {
             int handCash = stakeOfEveryDay;
+            int perDayWin = 0;
+            int perDayLoss = 0;
             while (handCash > 50 && handCash < 150) {
                 int bit = (int) (Math.random() * (2) + 1);
-                if (bit == win) {
+                if (bit == WIN) {
                     System.out.println("You are win");
                     handCash++;
+                    perDayWin++;
                 } else {
                     System.out.println("You are Loss");
                     handCash--;
+                    perDayLoss++;
                 }
                 System.out.println("Now your stake is " + handCash);
+                System.out.println("Your Have Win "+perDayWin+" bet and you have loss "+perDayLoss+" bet");
             }
             if (handCash == 50) {
                 System.out.println("You have loss 50% stake");
+                winDays++;
             } else {
                 System.out.println("You Have Win 50% stake");
+                lossDays++;
             }
         }
     }
