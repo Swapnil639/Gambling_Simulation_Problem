6853440324900024189a077a3e31a565e070bba0
Swapnil639 <swapnilpatil631997@gmail.com>
2023-07-11 10:21:41 +0530

    Added :Check win or loss using random

--- src/com/bridgelabz/GamblingGame.java
+++ src/com/bridgelabz/GamblingGame.java
@@ -1,13 +1,27 @@
 package com.bridgelabz;
 
-public class GamblingGame {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Gambling Simulation Program");
-        double stake = 100.0; // starting stake
-        double bet = 1.0; // bet amount
-        int gamesPlayed = 0; // number of games played
-        int gamesWon = 0; // number of games won
+import java.util.Random;
 
-        System.out.println("Stake of everyday:"+stake+" Bet every game:"+bet);
+public class GamblingGame {
+    public static int stakeOfEveryDay = 100;
+    public static final int betEveryGame = 1;
+    public static final int win = 1;
+
+    public static void main(String[] args) {
+        System.out.println("Welcome the Gambling Simulation Problem");
+        playGame();
+    }
+
+    public static void playGame() {
+        Random random=new Random();
+        int bit=random.nextInt(2);
+        if (bit == win) {
+            System.out.println("You are win");
+            stakeOfEveryDay++;
+        } else {
+            System.out.println("You are Loss");
+            stakeOfEveryDay--;
+        }
+        System.out.println("Now your stake is " + stakeOfEveryDay);
     }
 }
